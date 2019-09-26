ab679f8b49be0522a81f176ae2f88439d0b5a235
Noodles! <nnoodle@chiru.no>
2019-09-26 15:57:22 +0700

    Added saving

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const decompress = dat => JSON.parse(LZString.decompressFromBase64(dat))
 // const compress = dat => base64js.fromByteArray(LZMA.compress(JSON.stringify(dat)))
 // const decompress = dat => JSON.parse(LZMA.decompress(base64js.toByteArray(dat)))
 
+if (!localStorage.getItem('savedTables'))
+  localStorage.setItem('savedTables', compress({}))
+
 var app = new Vue({
   el: '#app',
   data: {
     dice: '2d6',
     result: '',
     t: {
-      name: 'Trinkets sold in Cart Market',
+      name: 'a few trinkets',
       amount: 1,
       sides: 20,
       data: [
@@ -36,20 +39,36 @@ var app = new Vue({
         { dc:20, val: "Mason jars filled with odd, dangerous-looking leeches." },
       ]
     },
+    savedTables: {},
+    rollCurrentTable: true,
+  },
+
+  computed: {
+    hash: function() {
+      return compress(scrunch(this.t))
+    },
+  },
+
+  watch: {
+    't.name': updateLink,
+    savedTables: function(tbls) {
+      localStorage.setItem('savedTables', compress(tbls))
+    },
   },
 
   beforeMount: function() {
     const hash = window.location.hash.substring(1)
     if (hash)
-      this.t = unscrunch(decompress(hash))
-    this.dice = this.t.amount+'d'+this.t.sides
-  },
-
-  watch: {
-    't.name': updateLink
+      try { this.t = unscrunch(decompress(hash)) } catch(e) {}
+    this.syncDiceView()
+    this.savedTables = decompress(localStorage.getItem('savedTables'))
   },
 
   methods: {
+    syncDiceView: function() {
+      this.dice = this.t.amount+'d'+this.t.sides
+    },
+
     updateDice: function(e) {
       const val = e.target.value
       let [x, s] = parseDice(val, this.t.amount, this.t.sides)
@@ -67,6 +86,7 @@ var app = new Vue({
         i = Number(i)
         this.t.data[i].dc = i+x
       }
+
       updateLink(this.t)
     },
 
@@ -75,15 +95,53 @@ var app = new Vue({
       updateLink(this.t)
     },
 
-    rollTable: function() {
-      const roll = rollxds(this.t.amount, this.t.sides)
-      this.result = this.t.data.find(({dc}) => dc === roll).val
+    doRoll: function() {
+      let results = []
+      if (this.rollCurrentTable)
+        results.push(rollTable(this.t.data, this.t.amount, this.t.sides))
+      Object.values(this.savedTables)
+        .filter(({roll}) => roll)
+        .forEach(({t}) => results.push(`[${t.name}] ${rollTable(t.data, t.amount, t.sides)}`))
+      this.result = results.length <= 1 ? results[0] : results
+    },
+
+    changeDC: function(e) {
+      let n = e.target.valueAsNumber
+      let o = parseInt(e.target.dataset.dc)
+      if (!n) return
+      this.t.data = this.t.data.filter(v => v.dc !== n)
+      this.t.data.find(v => v.dc === o).dc = n
+      e.target.dataset.dc = n
+
+      updateLink(this.t)
+    },
+
+    toggleRoll: function(name, e) {
+      this.savedTables[name].roll = (e.target.value === 'on')
+    },
+
+    saveTable: function() {
+      Vue.set(this.savedTables, this.t.name, {
+        name: this.t.name,
+        t: this.t,
+        link: compress(scrunch(this.t)),
+        roll: !!this.savedTables[this.t.name] && this.savedTables[this.t.name].roll,
+      })
+    },
+
+    loadTable: function(name) {
+      this.t = this.savedTables[name].t
+      this.syncDiceView()
+    },
+
+    deleteTable: function(name) {
+      Vue.delete(this.savedTables, name)
     },
   }
 })
 
 function updateLink(t) {
-  window.location.hash = '#'+compress(scrunch(this.t || t))
+  window.location.hash = '#' + (this.hash || compress(scrunch(t)))
 }
 
 function parseDice(str, da, ds) {
@@ -99,6 +157,14 @@ function rollxds(x, s) {
   while (x--)
     t += Math.floor(Math.random() * s) + 1
   return t
+}
+
+function rollTable(table, amount, sides) {
+  const roll = rollxds(amount, sides)
+  const list = [{dc: 0, val: '*nothing*'}]
+        .concat(table)
+        .concat([{dc: Infinity, val: 'infinite jest'}])
+  return list.find(({dc}, i, l) => l[i+1].dc > roll).val
 }
 
 function scrunch (t) {
